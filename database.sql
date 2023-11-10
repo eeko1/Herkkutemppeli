6e8b71e54c8f6de4e5a0ab1a6b06ce28508908a0
eeko1 <eemi.korhonen@metropolia.fi>
2023-11-10 15:14:47 +0200

    orders table added

--- database.sql
+++ database.sql
@@ -40,12 +40,37 @@ CREATE TABLE Coupons (
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
+CREATE TABLE Orders (
+  order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
 -- insert data
 INSERT INTO User_level (user_level_name) 
   VALUES ('User'), ('Admin'), ('Banned');
 
-INSERT INTO Users (fullname, password, email, phonenumber, profile_picture, user_level_id) 
-  VALUES ('Kamilla Karenius', 'Salasana90', 'kami@metro.fi', '234 5677890', 'profilepic.png', 1);
+-- Insert data into Users table
+INSERT INTO Users (fullname, password, email, phonenumber, profile_picture, user_level_id)
+VALUES
+('Admin User', 'adminpassword', 'admin@example.com', '123456789', 'admin.jpg', 1),
+('John Doe', 'userpassword', 'john@example.com', '987654321', 'john.jpg', 2),
+('Jane Doe', 'userpassword', 'jane@example.com', '555555555', 'jane.jpg', 2);
 
-INSERT INTO Products (product_name, product_description, product_image, product_category_id, product_allergens, product_price) 
-  VALUES ('Sliced Chicken Salad', 'Tender grilled chicken breast served atop a bed of crisp greens, cherry tomatoes, and zesty Italian dressing.', 'chickenSalad.jpg', 1, 'G, L, M, P, E, S', 13.90);
+-- Insert data into Products table
+INSERT INTO Products (product_name, product_description, product_image, product_category_id, product_allergens, product_price)
+VALUES
+('Product 1', 'Description for Product 1', 'product1.jpg', 1, 'Allergen A, Allergen B', 19.99),
+('Product 2', 'Description for Product 2', 'product2.jpg', 2, 'Allergen C', 29.99),
+('Product 3', 'Description for Product 3', 'product3.jpg', 1, 'No Allergens', 9.99);
+
+-- Insert data into Orders table
+INSERT INTO Orders (user_id, product_id, quantity, order_date)
+VALUES
+(2, 1, 2, '2023-11-10 12:34:56'),
+(3, 2, 1, '2023-11-11 09:45:23'),
+(2, 3, 3, '2023-11-12 15:22:11');
