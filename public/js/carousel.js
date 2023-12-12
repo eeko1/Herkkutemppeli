b67a770b618d2889c8936abe482dbc0b590fe85f
jambolul <jami.naatanen@gmail.com>
2023-12-12 15:32:09 +0200

    nav
    - modifications to nav css
    - made hero carousel "cooler"
    - username max length set to 8
    - responsivity done

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,6 +1,27 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+function showSlides(n) {
+  const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
 function changeSlide(n) {
   showSlides((slideIndex += n));
 }
@@ -9,21 +30,6 @@ function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
-function showSlides(n) {
-  const slides = document.querySelectorAll(".slide");
-  const dots = document.querySelectorAll(".dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+const slideInterval = setInterval(() => {
+  changeSlide(1);
+}, 10000);
