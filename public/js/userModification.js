3289dc8a5d8a9c558642252ec4c128f495dfbcf6
Eemi <eemi.korhonen@metropolia.fi>
2023-12-13 16:38:12 +0200

    modifications made to closing button

--- public/js/userModification.js
+++ public/js/userModification.js
@@ -1,73 +1,71 @@
-const modifyUserModal = document.getElementById("modifyUserModal");
-const navbarUsername = document.getElementById("navbarUsername");
-const closeModifyUserBtn = document.getElementById("closeModifyUser");
+document.addEventListener("DOMContentLoaded", function() {
+  const modifyUserModal = document.getElementById("modifyUserModal");
+  const closeModifyUserBtn = document.getElementById("closeModifyUser");
 
-const closeSignupModal = () => {
-  registerSignupDialog.close();
-};
+  closeModifyUserBtn.addEventListener("click", () => {
+    modifyUserModal.close();
+  });
 
-const openModifyUserModal = () => {
-  closeSignupModal();
-  modifyUserModal.showModal();
-};
+  // Your existing code
+  const navbarUsername = document.getElementById("navbarUsername");
 
-navbarUsername.addEventListener("click", openModifyUserModal);
-
-closeModifyUserBtn.addEventListener("click", () => {
-  modifyUserModal.close();
-  openRegisterDialog();
-});
-
-function modifyUsername() {
-  const newUsernameInput = document.getElementById("newUsername");
-}
-
-function modifyEmail() {
-  const newEmailInput = document.getElementById("newEmail");
-}
-
-function modifyPhoneNumber() {
-  const newPhoneNumberInput = document.getElementById("newPhoneNumber");
-}
-
-function modifyPassword() {
-  const newPasswordInput = document.getElementById("newPassword");
-}
-
-async function modifyAllFields() {
-  const newUsername = document.getElementById('newUsername').value;
-  const newEmail = document.getElementById('newEmail').value;
-  const newPhoneNumber = document.getElementById('newPhoneNumber').value;
-  const newPassword = document.getElementById('newPassword').value;
-
-  const formData = {
-    newUsername,
-    newEmail,
-    newPhoneNumber,
-    newPassword
+  const openModifyUserModal = () => {
+    modifyUserModal.showModal();
   };
 
-  try {
-    const response = await fetch('/user/update', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    });
+  navbarUsername.addEventListener("click", openModifyUserModal);
 
-    if (response.ok) {
-      alert('User information updated successfully');
-      // Additional actions upon successful update
-    } else {
-      alert('Error updating user information');
-    }
-  } catch (error) {
-    console.error('Error during fetch operation:', error);
-    alert('An error occurred while updating user information');
+  function modifyUsername() {
+    const newUsernameInput = document.getElementById("newUsername");
   }
-}
 
-function saveChanges() {
-  modifyAllFields();
-}
+  function modifyEmail() {
+    const newEmailInput = document.getElementById("newEmail");
+  }
+
+  function modifyPhoneNumber() {
+    const newPhoneNumberInput = document.getElementById("newPhoneNumber");
+  }
+
+  function modifyPassword() {
+    const newPasswordInput = document.getElementById("newPassword");
+  }
+
+  async function modifyAllFields() {
+    const newUsername = document.getElementById('newUsername').value;
+    const newEmail = document.getElementById('newEmail').value;
+    const newPhoneNumber = document.getElementById('newPhoneNumber').value;
+    const newPassword = document.getElementById('newPassword').value;
+
+    const formData = {
+      newUsername,
+      newEmail,
+      newPhoneNumber,
+      newPassword
+    };
+
+    try {
+      const response = await fetch('/user/update', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (response.ok) {
+        alert('User information updated successfully');
+        // Additional actions upon successful update
+      } else {
+        alert('Error updating user information');
+      }
+    } catch (error) {
+      console.error('Error during fetch operation:', error);
+      alert('An error occurred while updating user information');
+    }
+  }
+
+  function saveChanges() {
+    modifyAllFields();
+  }
+});
